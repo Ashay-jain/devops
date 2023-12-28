4d587d06f1a20ce87d809fccb73fe2894ba369a4
Administrator <ashay.jain@systematixindia.com>
2023-12-28 22:58:33 +0530

    added remote state backend and statelocking with dynamodb

--- ec2-creation/providers.tf
+++ ec2-creation/providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
 }
+# remote state storing on s3 and state file locking using dynamodb
+terraform {
+  backend "s3" {
+    bucket = "terraform-s3-aj"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    dynamodb_table = "statelocking-tf-aj"
+  }
+}
